e65ed2ab38c34b554b8d028c9716f48fb6c58016
GermanGaming <39227403+GermanGaming@users.noreply.github.com>
2020-06-12 12:26:17 +0200

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,27 @@
+Locales ['de'] = {
+  -- Prisoner Clothing
+  ['clothing_menu'] = 'Wähl deine Kleidung',
+  ['citizen_wear'] = 'Zivilbekleidung',
+  ['jail_wear'] = 'Häftlingskleidung',
+  ['press_access'] = 'Drücke ~INPUT_CONTEXT~ um die Kleidung zu wechseln.',
+  -- Jail
+  ['judge'] = 'RICHTER',
+  ['jailed_msg'] = '%s ist jetzt für %s im Gefängnis.',
+  ['unjailed_msg'] = '%s ist aus dem Gefängnis entlassen worden!',
+  ['player_escaped'] = '~r~%s ~s~ist entkommen aus ~r~%s ~s~dem Gefängnis!!!!',
+  ['player_escape_attempt'] = '~r~%s ~s~hat versucht, aus dem ~r~%s ~s~Gefängnis zu fliehen!!!',
+  ['escape_attempt'] = 'Du darfst NICHT aus dem Gefängnis fliehen!',
+  ['remaining_msg'] = 'Es bleiben ~b~%s~s~ Sekunden, bis Sie aus dem Gefängnis entlassen werden.',
+  ['you_ran'] = '~r~Du bist aus dem Gefängnis geflohen!',
+  -- Add to esx_policejob
+  ['jail'] = 'Gefängnis',
+  ['jail_menu_info'] = 'Gefängniszeit in Minuten',
+  ['invalid_amount'] = 'Ungültiger Zeitraum. Beispiel: 5 = 5 Minuten',
+  ['jail_menu'] = 'Gefängnisstandort wählen',
+  ['mission_row'] = 'Mission Row',
+  ['bolingbroke_pen'] = 'Bolingbroke Penitentiary',
+  ['mission_row_1'] = 'Mission Row Zelle 1',
+  ['mission_row_2'] = 'Mission Row Zelle 2',
+  ['mission_row_3'] = 'Mission Row Zelle 3',
+  ['bolingbroke_pen_1'] = 'Bolingbroke Penitentiary Yard',
+}
